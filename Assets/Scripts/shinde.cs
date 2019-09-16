70f1d2e9503aa61bab250f7022711102997b3fab
D3no <deen.dupreez@gmail.com>
2019-09-16 11:51:34 +0200

    The basic level scene

---
+++ Assets/Scripts/shinde.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class shinde : MonoBehaviour
+{
+
+    // Use this for initialization
+    void OnCollision(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("levelTest");
+            Debug.Log("working");
+        }
+    }
+
+}
+
