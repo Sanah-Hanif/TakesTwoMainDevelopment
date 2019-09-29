6b9d6024a74fad19d06696777afbb24f1a46bdc8
Duncan <duncan.smale@gmail.com>
2019-09-29 20:08:08 +0200

    Prefab changes, added change scene stuff.

---
+++ Assets/Scripts/Exit/ExitSceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Exit
+{
+    public class ExitSceneLoader : MonoBehaviour
+    {
+        [SerializeField] private string sceneToLoad;
+        
+        private List<GameObject> _players = new List<GameObject>();
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(!other.gameObject.tag.Equals("Player")) return;
+            _players.Add(other.gameObject);
+            if (_players.Count == 2)
+                LoadNextScene();
+        }
+
+        private void LoadNextScene()
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if(!other.gameObject.tag.Equals("Player")) return;
+            _players.Remove(other.gameObject);
+        }
+    }
+}
