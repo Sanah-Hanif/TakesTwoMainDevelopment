32ac5b751e63e5697ce1e26b729fbc7314126195
Duncan <duncan.smale@gmail.com>
2019-09-29 21:37:40 +0200

    Ground work for scene loading started.

--- Assets/Scripts/Scene/SceneLoader.cs
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -1,9 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Scene
 {
     public class SceneLoader : MonoBehaviour
     {
-        
+
+        private string _currentScene;
+        private string _nextScene;
+
+
+        public IEnumerator LoadNewScene(string newScene)
+        {
+            
+            /* TODO: add scene loading logic
+             * TODO: ensure the same scene doesnt load twice
+             * TODO: unload previous scene
+             * TODO: load new scene
+             * TODO: fades/cuts between puzzles
+             */
+            
+            return null;
+        }
     }
 }
