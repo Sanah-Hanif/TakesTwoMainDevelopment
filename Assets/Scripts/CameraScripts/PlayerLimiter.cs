b043f9fa0aebfb7f4f6e0cc18388aa1b0bbb429e
Sanah-Hanif <saneebanee@yahoo.com>
2019-09-29 20:10:33 +0200

    Worked on the size limitation thing which is more or less working

--- Assets/Scripts/CameraScripts/PlayerLimiter.cs
+++ Assets/Scripts/CameraScripts/PlayerLimiter.cs
@@ -1,31 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerLimiter : MonoBehaviour
 {
     private Camera _camera;
+    public CinemachineVirtualCamera vCam;
 
     [SerializeField] private Rigidbody2D _player;
 
     private float _size;
     private float _ratio;
+    [SerializeField] float range;
 
     void Start()
     {
         _camera = Camera.main;
-        _size = _camera.orthographicSize;
-        _ratio = _camera.aspect;
+        _size = vCam.m_Lens.OrthographicSize;
+        _ratio = vCam.m_Lens.Aspect;
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        _size = _camera.orthographicSize;
-        _ratio = _camera.aspect;
+        
+        //_ratio = _camera.aspect;
         Vector2 position = Vector2.zero;
-        position.x = Mathf.Clamp(_player.position.x, _ratio * _size * -1, _ratio * _size);
-        position.y = Mathf.Clamp(_player.position.y, -_size, _size);
+        position.x = Mathf.Clamp(_player.position.x, this.transform.position.x -_size * _ratio * range, this.transform.position.x + _size * _ratio * range);
+        position.y = Mathf.Clamp(_player.position.y, this.transform.position.y -_size, this.transform.position.y + _size * 2);
         _player.position = position;
     }
 }
