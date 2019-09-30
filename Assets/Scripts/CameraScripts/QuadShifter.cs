adab64422429412dd27459062a6bc265e273fb4c
Sanah-Hanif <saneebanee@yahoo.com>
2019-09-30 19:46:48 +0200

    Finally have parallax working :D

--- Assets/Scripts/CameraScripts/QuadShifter.cs
+++ Assets/Scripts/CameraScripts/QuadShifter.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class QuadShifter : MonoBehaviour
 {
-    [SerializeField] float speed;
-    [SerializeField] Renderer rend;
+    [SerializeField] Transform followingPoint;
+    Vector2 currentPos, prevPos;
+    float offsetx, offsety;
+
+    [SerializeField] float speedx, speedy;
+    Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = this.GetComponent<Renderer>();
+        currentPos = new Vector2(followingPoint.position.x, followingPoint.position.y);
+        prevPos = currentPos;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.P))
-        {
-            float offset = Time.time * speed;
-            rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
-            rend.material.SetTextureOffset("_BaseMap", new Vector2(offset, 0));
-        }
+        currentPos = new Vector2(followingPoint.position.x, followingPoint.position.y);
+        float dist = currentPos.x - prevPos.x;
+        float dist2 = currentPos.y - prevPos.y;
+        offsetx += dist * speedx * Time.deltaTime;
+        offsety += dist2 * speedy * Time.deltaTime;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offsetx, offsety));
+        rend.material.SetTextureOffset("_BaseMap", new Vector2(offsetx, offsety));
+
+        prevPos = currentPos;
+        
     }
 }
