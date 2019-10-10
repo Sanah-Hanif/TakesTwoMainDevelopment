437afaf6cb6e6406fb94ebcea44d1b85ad39b914
Duncan <duncan.smale@gmail.com>
2019-10-10 11:24:09 +0200

    Made targetgroupcontroller for better camera panning/zooming

---
+++ Assets/Scripts/CameraScripts/TargetGroupController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using DG.Tweening;
+using UnityEngine;
+
+namespace CameraScripts
+{
+    public class TargetGroupController : MonoBehaviour
+    {
+        [SerializeField] private float startWeight = 2.5f, startRadius = 2.5f, endWeight = 5f, endRadius = 5f, lerpDuration = 2f;
+        private CinemachineTargetGroup _target;
+        private Dictionary<GameObject, IEnumerator> _routines = new Dictionary<GameObject, IEnumerator>();
+
+        private void Awake()
+        {
+            _target = GetComponent<CinemachineTargetGroup>();
+        }
+
+        public void AddObjectToTargetGroup(GameObject objToAdd)
+        {
+            var routine = IncreaseWeight(objToAdd);
+            if(_routines.ContainsKey(objToAdd)) return;
+            _routines.Add(objToAdd, routine);
+            StartCoroutine(routine);
+        }
+
+        public void RemoveObjectFromTargetGroup(GameObject objToRemove)
+        {
+            
+        }
+
+        private IEnumerator IncreaseWeight(GameObject objToIncrease)
+        {
+            
+            var currentWeight = startWeight;
+            var currentWeightLerp = 0f;
+            var currentRadius = startRadius;
+            var currentRadiusLerp = 0f;
+            _target.AddMember(objToIncrease.transform, currentWeight, currentRadius);
+            var member = _target.FindMember(objToIncrease.transform);
+            
+            var endFrame = new WaitForEndOfFrame();
+            
+            yield return endFrame;
+            
+            while (currentRadiusLerp < 1 && currentWeightLerp < 1)
+            {
+                //currentRadius += Time.deltaTime * (endRadius - startRadius) * (1f/lerpDuration);
+                currentRadiusLerp += Time.deltaTime * (endRadius - startRadius) * (1f/lerpDuration);
+                //currentWeight += Time.deltaTime * (endWeight - startWeight) * (1f/lerpDuration);
+                currentWeightLerp += Time.deltaTime * (endWeight - startWeight) * (1f/lerpDuration);
+                
+                
+                _target.m_Targets[member].radius = Mathf.Lerp(startRadius, endRadius, currentRadiusLerp);
+                _target.m_Targets[member].weight = Mathf.Lerp(startWeight, endWeight, currentWeightLerp);
+                
+                yield return endFrame;
+            }
+
+            currentRadiusLerp = 1;
+            currentWeightLerp = 1;
+
+            yield return new WaitForSeconds(1f);
+            
+            while (currentWeightLerp > 0 && currentRadiusLerp > 0)
+            {
+                currentRadiusLerp -= Time.deltaTime * (endRadius - startRadius) * (1f/lerpDuration);
+                currentWeightLerp -= Time.deltaTime * (endWeight - startWeight) * (1f/lerpDuration);
+                
+                
+                _target.m_Targets[member].radius = Mathf.Lerp(startRadius, endRadius, currentRadiusLerp);
+                _target.m_Targets[member].weight = Mathf.Lerp(startWeight, endWeight, currentWeightLerp);
+                
+                yield return endFrame;
+            }
+            
+            _target.RemoveMember(objToIncrease.transform);
+            _routines.Remove(objToIncrease);
+
+        }
+
+        /*private IEnumerator DecreaseWeight(GameObject objToDecrease)
+        {
+            
+        }*/
+    }
+}
--- Assets/Scripts/Interaction/Level Elements/EnvironmentInteraction.cs
+++ Assets/Scripts/Interaction/Level Elements/EnvironmentInteraction.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using CameraScripts;
 using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -16,11 +17,13 @@ namespace Interaction.Level_Elements
         protected InputActionMap PlayerAbility;
 
         private CinemachineTargetGroup target;
+        private TargetGroupController _targetGroupController;
 
         private void Awake()
         {
             target = FindObjectOfType<CinemachineTargetGroup>();
             child = GetComponentInChildren<LevelInteraction>();
+            _targetGroupController = target.GetComponent<TargetGroupController>();
         }
 
         public override void Interact()
@@ -33,21 +36,22 @@ namespace Interaction.Level_Elements
             {
                 //Debug.Log(target.FindMember(interaction.transform));
                 if(target.FindMember(interaction.transform) == -1)
-                    target.AddMember(interaction.transform, 2.5f, 2.5f);
+                    //target.AddMember(interaction.transform, 2.5f, 2.5f);
+                    _targetGroupController.AddObjectToTargetGroup(interaction.gameObject);
                 interaction.Interact();
             }
             
-            Invoke(nameof(RemoveTarget), 2f);
+            //_targetGroupController.AddObjectToTargetGroup();
+            //Invoke(nameof(RemoveTarget), 2f);
         }
 
-        void RemoveTarget()
+        private void RemoveTarget()
         {
             foreach (var interaction in dependancies.Where(interaction => interaction))
             {
                 if(target.FindMember(interaction.transform) != -1)
                     target.RemoveMember(interaction.transform);
             }
-            
         }
         
     }
