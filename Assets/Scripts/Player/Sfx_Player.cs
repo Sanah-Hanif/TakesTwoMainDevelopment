7c428433c7d55edeaccb89988ca2ec9c5e69cc9b
D3no <deen.dupreez@gmail.com>
2019-10-31 11:22:53 +0200

    Just Working in Animation Scene(sound implementation, nothing proper yet, still sorting out event scripts etc)

---
+++ Assets/Scripts/Player/Sfx_Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sfx_Player : MonoBehaviour
+{
+    public AudioClip footsteps;
+    public AudioSource audioS;
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    void Footsteps()
+    {
+        audioS.PlayOneShot(footsteps);
+    }
+}
