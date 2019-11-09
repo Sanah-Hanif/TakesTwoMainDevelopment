85dc32685e6a7e6d7992b9ada0bd2c268cae9103
D3no <deen.dupreez@gmail.com>
2019-11-09 02:03:30 +0200

    Default Jump and Run Sounds Ready ! (Applied to player prefabs)

--- Assets/Scripts/Player/Sfx_Player.cs
+++ Assets/Scripts/Player/Sfx_Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Sfx_Player : MonoBehaviour
 {
     public AudioClip footsteps;
+    public AudioClip jumpSfx;
     public AudioSource audioS;
    
     void Start()
@@ -19,5 +20,9 @@ public class Sfx_Player : MonoBehaviour
     void Footsteps()
     {
         audioS.PlayOneShot(footsteps);
+    }
+    void Jump()
+    {
+        audioS.PlayOneShot(jumpSfx);
     }
 }
